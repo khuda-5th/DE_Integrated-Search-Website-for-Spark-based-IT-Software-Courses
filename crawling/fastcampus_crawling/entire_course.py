9122c7b78c4d7e9909ce8f3eeaa7fcc37da3e861
GU-0 <rjsgud5386@gmail.com>
2024-05-16 19:52:38 +0900

    Complete

--- crawling/fastcampus_crawling/entire_course.py
+++ crawling/fastcampus_crawling/entire_course.py
@@ -1,27 +1,32 @@
-from utilities import *
+from fastcampus_selenium_driver import *
 from course_items import *
+
+
+def save_json(path, file_name, data):
+    with open(f"{path}/{file_name}.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
 
 start_time = datetime.now()
 formatted_time = start_time.strftime("%y%m%d%H%M")
 LOG_NAME = f"{formatted_time}_fastcampus.log"
-JSON_NAME = f"{formatted_time}_fastcampus.json"
+OUTPUT_NAME = f"{formatted_time}_fastcampus.json"
 
 logging.basicConfig(
     level=logging.INFO,
-    filename=f"logs/{LOG_NAME}",
-    filemode="w",
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler(f"entire_logs/{LOG_NAME}"), logging.StreamHandler()],
 )
 logging.info("Crawl Entire Courses Start")
-###############################################################################################
+###################################################################################################################
 
-driver = start_selenium("https://fastcampus.co.kr/categories")
+driver = FastcampusSeleniumDriver("https://fastcampus.co.kr/categories")
 # 메뉴 버튼 클릭
-wait_click_until_located(driver, 1, By.CLASS_NAME, "header-new__mobile-menu-icon")
+driver.wait_click_until_located(10, By.CLASS_NAME, "header-new__mobile-menu-icon")
 # 대분류/소분류 정보 수집
-category_url = parse_category(driver)
+category_url = driver.parse_category()
 # 내보낼 데이터
-output = {"categories": []}
+output = FastcampusItem()
 # 소분류 페이지로 이동
 for category in list(category_url.keys()):
     # CategoryItem 생성
@@ -29,71 +34,64 @@ for category in list(category_url.keys()):
     for sub_category, sub_url in category_url[category]:
         # SubcategoryItem 생성
         subcategory_item = SubCategoryItem(sub_category)
-        driver.get(sub_url)
+        driver.driver.get(sub_url)
         logging.info(
             "========================================================================================================"
         )
         logging.info(f"{category}: {sub_category} Start")
-        count_contents = infinite_scroll(driver)
+        count_contents = driver.infinite_scroll()
         logging.info(f"{category}: {sub_category} Count-{count_contents}")
 
         # 강의 요소들
-        courses = driver.find_elements(
+        courses = driver.driver.find_elements(
             By.CSS_SELECTOR, ".infinity-course .course-card__container"
         )
         for course in courses:
             course_item = CourseItem()
             # 강의 뱃지 (전체오픈, 사전예약 등등..)
-            course_badge = parse_course_badge(driver, course)
+            course_badge = driver.parse_course_badge(course)
             course_item.badge = course_badge
             # 강의 제목
-            course_title, course_intro = parse_course_title_intro(driver, course)
+            course_title, course_intro = driver.parse_course_title_intro(course)
             course_item.title = course_title
             course_item.intro = course_intro
             # 강의 태그
-            course_tags = parse_course_tags(driver, course)
+            course_tags = driver.parse_course_tags(course)
             course_item.tags = course_tags
             # 강의 이미지 URL
-            course_img = parse_course_img(driver, course)
+            course_img = driver.parse_course_img(course)
             course_item.course_img = course_img
             # 강의 세부 페이지 URL
-            course_url = parse_course_url(driver, course)
+            course_url = driver.parse_course_url(course)
             course_item.course_url = course_url
             # SubcategoryItem에 현재 Course 추가
             subcategory_item.courses.append(course_item)
         # 각 강의별 상세 페이지 이동
-        cnt = 0
         for course in subcategory_item.courses:
-            cnt += 1
-            logging.info(f"{category}: {sub_category} {cnt}. {course.title} Start")
-            navigate_course_detail(driver, course.course_url)
+            logging.info(f"{category}: {sub_category} - {course.title} Start")
+            driver.navigate_course_detail(course.course_url)
             # 가격
-            regular_price, sale_price = parse_course_price(driver)
+            regular_price, sale_price = driver.parse_course_price()
             course.regular_price = regular_price
             course.sale_price = sale_price
             # 강의 요약 및 간단 소개
-            course_summary = parse_course_summary(driver)
+            course_summary = driver.parse_course_summary()
             course.summary = course_summary
             # 강의 Part 정보
-            course_parts = parse_course_parts(driver)
+            course_parts = driver.parse_course_parts()
             course.parts = course_parts
             # 강의 기본 정보, 특징, 커리큘럼 등의 정보
-            course_accordion = parse_course_accordion(driver)
+            course_accordion = driver.parse_course_accordion()
             course.accordion = course_accordion
         # CategoryItem에 SubCategory 정보 추가
         category_item.sub_categories.append(subcategory_item)
 
-        """   테스트용 임시저장   """
-        sub_category_json = sub_category.replace("/", "_")
-        with open(f"tmp/{sub_category_json}.json", "w", encoding="utf-8") as file:
-            json.dump(subcategory_item.to_dict(), file, ensure_ascii=False, indent=4)
-        """                   """
     # 최종 데이터에 category_item 추가
-    output["categories"].append(category_item.to_dict())
+    output.categories.append(category_item.to_dict())
+###################################################################################################################
 
-# 파일 출력
-with open(f"outputs/{JSON_NAME}", "w", encoding="utf-8") as file:
-    json.dump(output, file, ensure_ascii=False, indent=4)
+# 파일 저장
+save_json("entire_outputs", OUTPUT_NAME, output.to_dict())
 
 end_time = datetime.now()
 time_spent = end_time - start_time
@@ -109,3 +107,5 @@ else:
 
 logging.info(f"Completed at {end_time.strftime('%y%m%d%H%M')}")
 logging.info(f"Total {time_str}")
+
+driver.driver.quit()
