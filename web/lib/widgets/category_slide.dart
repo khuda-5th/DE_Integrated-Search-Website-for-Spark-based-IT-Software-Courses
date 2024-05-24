311cd4f02f693398698a77d4a9a2ada3ce757a62
myeunee <myeunee@gmail.com>
2024-05-25 05:32:17 +0900

    Add web directory as a regular directory

---
+++ web/lib/widgets/category_slide.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'category_button.dart';
+
+class CategorySlide extends StatelessWidget {
+  final List<String> selectedCategories;
+  final Function(String) onCategorySelected;
+
+  CategorySlide({required this.selectedCategories, required this.onCategorySelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      color: Colors.white, // 카테고리 슬라이드 배경을 흰색으로 설정
+      child: ListView(
+        children: [
+          CategoryButton(
+            category: '추천순',
+            isSelected: selectedCategories.contains('추천순'),
+            onCategorySelected: onCategorySelected,
+          ),
+          CategoryButton(
+            category: '낮은 가격순',
+            isSelected: selectedCategories.contains('낮은 가격순'),
+            onCategorySelected: onCategorySelected,
+          ),
+          CategoryButton(
+            category: '높은 가격순',
+            isSelected: selectedCategories.contains('높은 가격순'),
+            onCategorySelected: onCategorySelected,
+          ),
+          // 추가 카테고리들
+        ],
+      ),
+    );
+  }
+}
---
+++ web/lib/widgets/course_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../models/course_model.dart';
+import '../services/api_service.dart';
+import 'course_item.dart';
+
+class CourseList extends StatelessWidget {
+  final ApiService apiService = ApiService();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Course>>(
+      future: apiService.fetchCourses(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData) {
+          final courses = snapshot.data ?? [];
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4, // 열의 개수를 조정할 수 있습니다.
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 10.0,
+                childAspectRatio: 5/4,
+              ),
+              itemCount: courses.length,
+              itemBuilder: (context, index) {
+                final course = courses[index];
+                return CourseItem(course: course); // course 하나만 나오는
+              },
+            ),
+          );
+        } else {
+          return Center(child: Text('No data available'));
+        }
+      },
+    );
+  }
+}
